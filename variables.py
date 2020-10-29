b0adc0b97d84ecadd10612cc76e372e6c8bbeb05
feorpa <72934355+feorpa@users.noreply.github.com>
2020-10-29 11:56:59 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,43 @@
+maasAli = 5000
+maasAhmet = 4000
+vergi = 0.27
+
+print (maasAli - (maasAli*vergi))
+print (maasAhmet - (maasAhmet*vergi))
+
+# Değişken Tanımlama Kuralları
+
+# rakam ile başlayamaz. 
+
+number1 = 10
+
+# Büyük küçük harf duyarlılığı vardır.
+
+age = 20
+Age = 25
+print (age)
+print (Age)
+
+#Türkçe karakter kullanmayalım.
+
+yas=20
+
+x = 1          #int
+y = 2.3        #float
+name = "Ferit" #string
+isStudent = True #bool
+
+# x, y, name, isStudent = (1, 2.3, "Orçun, True")
+
+a = 10
+b = 20
+print(a+b) #30
+
+a = '10'
+b = '20'
+print(a+b) #1020
+
+FirstName = "Ferit"
+lastName = " Parlak"
+
+print (FirstName + lastName)
